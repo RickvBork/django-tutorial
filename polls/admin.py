b1053362ae1c029271089c48ab2118a2bcc395b7
Rick van Bork <rick-627@hotmail.com>
2020-01-13 15:51:53 +0100

    Added admin functionality

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
+"""
+Handles the way choices are displayed in the change Question admin.
+"""
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 
 class ChoiceModelAdmin(admin.ModelAdmin):
     """
@@ -18,13 +25,32 @@ class QuestionModelAdmin(admin.ModelAdmin):
     Adds extra information columns to the admin list for Choices.
     Now the associated choices are also displayed per question.
     """
-    list_display = ('__str__', 'grab_choice_set', 'id')
+    list_display = ('__str__', 'choices', 'pub_date', 'was_published_recently', 'id')
+
+    # Sets the amount of items that can be viewed per page
+    list_per_page = 10
+
+    # Allows filtering by publication date
+    list_filter = ['pub_date']
+
+    # Allows the admin to search by typing
+    search_fields = ['question_text']
+
+    # Force pub_date fields above the question_text field in: http://127.0.0.1:8000/admin/polls/question/'#id'/change/
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'],
+                              'classes': ['collapse']}),
+        ]
+
+    # Add choices to the question change admin
+    inlines = [ChoiceInline]
 
     """
     Grabs the choice set for the questions and forces it into a list.
     """
     @staticmethod
-    def grab_choice_set(question):
+    def choices(question):
         return list(question.choice_set.all())
 
 
